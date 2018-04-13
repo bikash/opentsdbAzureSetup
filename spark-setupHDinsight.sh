0586ecd6778c7fef85371a3d4cfbf95a85b21b67
Agrawal <Bikash.Agrawal@dnvgl.com>
2018-04-13 09:41:09 +0200

    clean up

--- spark-setupHDinsight.sh
+++ spark-setupHDinsight.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Updating existing symbolic links and cleanup existing spark1 binaries
+# Updating existing symbolic links and cleanup existing spark-1.6 binaries
 rm -r /usr/bin/pyspark
 rm -r /usr/bin/spark-class
 rm -r /usr/bin/spark-shell
@@ -13,29 +13,21 @@ ln -s /usr/hdp/current/spark2-client/bin/spark-class /usr/bin/
 ln -s /usr/hdp/current/spark2-client/bin/spark-submit /usr/bin/
 ln -s /usr/hdp/current/spark2-client/bin/spark-shell /usr/bin/
 ln -s /usr/hdp/current/spark2-client/bin/pyspark /usr/bin/
+ln -s /usr/hdp/current/spark2-client /usr/hdp/current/spark-client
 
 
-echo "export SPARK_HOME=/usr/hdp/current/spark2-client" >> /home/sshuser/.bashrc
-
-
-
-sudo ln -s /usr/hdp/current/spark2-client /usr/hdp/current/spark-client
-
-#wget --no-check-certificate  https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh
-
-#bash Anaconda2-5.1.0-Linux-x86_64.sh
-
-#export PATH=/home/sshuser/anaconda2/bin:$PATH
 echo "export SPARK_HOME=/usr/hdp/current/spark2-client" >> /home/sshuser/.bashrc
 echo "export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH" >> /home/sshuser/.bashrc
 echo "export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.3-src.zip:$PYTHONPATH" >> /home/sshuser/.bashrc
 echo "export PYSPARK_PYTHON=/usr/bin/python" >> /home/sshuser/.bashrc
 #export PYTHONPATH=/usr/hdp/current/spark2-client/python/lib/pyspark.zip:/usr/hdp/current/spark2-client/python/lib/py4j-0.10.3-src.zip
 
+
 pip install --upgrade pip
-pip install pandas
-pip install requests
 
 # Updating PATH with spark bin
 NEWPATH=$PATH:/usr/hdp/current/spark2-client/bin
 sed -i "s|^PATH=.*|PATH=\"$NEWPATH\"|g" /etc/environment
+
+
+source /home/sshuser/.bashrc
